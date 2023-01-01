e5cd5f741aef4488cb5cef826a96cae20693cd61
Charles Julian Knight <charles@rabidaudio.com>
2023-01-01 17:26:03 -0500

    initial commit

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import LastFM from "last-fm"
+console.log("hello")
+
+const canvas = document.createElement('canvas')
+document.body.appendChild(canvas);
+const ctx = canvas.getContext('2d')
+
+ctx.fillStyle = 'green'
+ctx.fillRect(10, 10, 150, 100)
